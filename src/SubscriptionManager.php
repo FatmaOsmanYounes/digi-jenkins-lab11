cf7e7a31167003d1a7b67fc0844ce9de74ac871b
fatma <fatmaosmanyounes@gmail.com>
2026-05-11 12:01:46 +0300

    fix subscription calculation

--- src/SubscriptionManager.php
+++ src/SubscriptionManager.php
@@ -3,10 +3,6 @@ namespace App;
 
 class SubscriptionManager {
     public function getDaysRemaining($totalDays, $daysUsed) {
-        if ($daysUsed != $totalDays) {
-            return 0;
-        }
-        
         return $totalDays - $daysUsed;
     }
 }
